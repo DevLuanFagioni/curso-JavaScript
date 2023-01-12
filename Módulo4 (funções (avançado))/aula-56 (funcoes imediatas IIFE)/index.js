1bb21e33ed9086374d507f5c18a3daa7823e78fc
Luan Fagioni <101909254+DevLuanFagioni@users.noreply.github.com>
2023-01-12 10:54:41 -0300

    upload aula-56

---
+++ Módulo4 (funções (avançado))/aula-56 (funcoes imediatas IIFE)/index.js
@@ -0,0 +1,12 @@
+// IIFE -> Immediately invoked function expression
+// criar e executar a funcao imediatamente
+// (function(){
+
+// })()
+
+
+(function (idade, peso, altura) {
+    
+    return console.log(idade, peso, altura);
+
+})(30, 80, 1.80);
